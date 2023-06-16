744f6097a41db10874ee2826b016ce56f1441b42
Grzegorz Piotrowski <gpiotrow@redhat.com>
2023-06-16 17:12:49 +0100

    Exercise 06_02b completed

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response type: %T", resp)
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(bytes)
+	fmt.Print(content)
+
 	fmt.Println("Network requests")
 }
