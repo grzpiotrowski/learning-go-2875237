31b478f38686e0d39e63ecc3a30a4ec8267d5bd6
Grzegorz Piotrowski <gpiotrow@redhat.com>
2023-06-16 16:29:36 +0100

    Exercise 05_01b completed

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+	sum := addValues(5, 7)
+	fmt.Println("The sum is", sum)
+
+	multiSum, multiCount := addAllValues(3, 7, 12)
+	fmt.Println("Sum of multiple values is:", multiSum)
+	fmt.Println("Count of items:", multiCount)
+}
+
+func doSomething() {
+	fmt.Println("Doing Something")
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+	for _, value := range values {
+		total += value
+	}
+	return total, len(values)
 }
